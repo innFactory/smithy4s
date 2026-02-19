52b6ed469847f06e6c8cc4726a258bcf2bdcf4ac
yisraelu <ysrlunion@gmail.com>
2026-02-18 23:58:47 -0500

    fixes

--- modules/mill-codegen-plugin/src-mill-shared/smithy4s/codegen/mill/Smithy4sModuleCommon.scala
+++ modules/mill-codegen-plugin/src-mill-shared/smithy4s/codegen/mill/Smithy4sModuleCommon.scala
@@ -98,8 +98,7 @@ trait Smithy4sModuleCommon extends ScalaModule {
   def smithy4sTransitiveIvyDeps: T[Agg[Dep]] = T {
     smithy4sAllDeps() ++ T
       .traverse(moduleDeps) {
-        case m if m.isInstanceOf[Smithy4sModule] =>
-          m.asInstanceOf[Smithy4sModule].smithy4sTransitiveIvyDeps
+        case m: Smithy4sModule => m.smithy4sTransitiveIvyDeps
         case _ => T.task(mill.api.Result.create(Agg.empty))
       }()
       .flatten
