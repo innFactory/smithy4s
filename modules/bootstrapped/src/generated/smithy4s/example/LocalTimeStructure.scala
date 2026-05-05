d7163a090ad5533de8c61fd9de3f9b4d27da6c54
Antonio Morales <Antonio.Morales.-ND@disney.com>
2026-05-05 13:20:44 -0700

    re-codegen sampleSpecs to show that things are fixed

--- modules/bootstrapped/src/generated/smithy4s/example/LocalTimeStructure.scala
+++ modules/bootstrapped/src/generated/smithy4s/example/LocalTimeStructure.scala
@@ -1,6 +1,5 @@
 package smithy4s.example
 
-import smithy.api.String
 import smithy4s.Hints
 import smithy4s.Schema
 import smithy4s.ShapeId
@@ -9,7 +8,7 @@ import smithy4s.schema.Schema.localtime
 import smithy4s.schema.Schema.struct
 import smithy4s.time.LocalTime
 
-final case class LocalTimeStructure(localTime: LocalTime, localTime2: MyLocalTime, localTime3: String)
+final case class LocalTimeStructure(localTime: LocalTime, localTime2: MyLocalTime, localTime3: LocalTime)
 
 object LocalTimeStructure extends ShapeTag.Companion[LocalTimeStructure] {
   val id: ShapeId = ShapeId("smithy4s.example", "LocalTimeStructure")
@@ -17,11 +16,11 @@ object LocalTimeStructure extends ShapeTag.Companion[LocalTimeStructure] {
   val hints: Hints = Hints.empty
 
   // constructor using the original order from the spec
-  private def make(localTime: LocalTime, localTime2: MyLocalTime, localTime3: String): LocalTimeStructure = LocalTimeStructure(localTime, localTime2, localTime3)
+  private def make(localTime: LocalTime, localTime2: MyLocalTime, localTime3: LocalTime): LocalTimeStructure = LocalTimeStructure(localTime, localTime2, localTime3)
 
   implicit val schema: Schema[LocalTimeStructure] = struct[LocalTimeStructure](
     localtime.required[LocalTimeStructure]("localTime", _.localTime),
     MyLocalTime.schema.required[LocalTimeStructure]("localTime2", _.localTime2),
-    String.schema.required[LocalTimeStructure]("localTime3", _.localTime3).addHints(Hints.dynamic(ShapeId("alloy", "localTimeFormat"), smithy4s.Document.obj())),
+    localtime.required[LocalTimeStructure]("localTime3", _.localTime3).addHints(Hints.dynamic(ShapeId("alloy", "localTimeFormat"), smithy4s.Document.obj())),
   )(make).withId(id).addHints(hints)
 }
--- modules/bootstrapped/src/generated/smithy4s/example/OffsetDateTimeStructure.scala
+++ modules/bootstrapped/src/generated/smithy4s/example/OffsetDateTimeStructure.scala
@@ -1,6 +1,5 @@
 package smithy4s.example
 
-import smithy.api.Timestamp
 import smithy4s.Hints
 import smithy4s.Schema
 import smithy4s.ShapeId
@@ -9,7 +8,7 @@ import smithy4s.schema.Schema.offsetdatetime
 import smithy4s.schema.Schema.struct
 import smithy4s.time.OffsetDateTime
 
-final case class OffsetDateTimeStructure(offsetDateTime: OffsetDateTime, offsetDateTime2: MyOffsetDateTime, offsetDateTime3: Timestamp)
+final case class OffsetDateTimeStructure(offsetDateTime: OffsetDateTime, offsetDateTime2: MyOffsetDateTime, offsetDateTime3: OffsetDateTime)
 
 object OffsetDateTimeStructure extends ShapeTag.Companion[OffsetDateTimeStructure] {
   val id: ShapeId = ShapeId("smithy4s.example", "OffsetDateTimeStructure")
@@ -17,11 +16,11 @@ object OffsetDateTimeStructure extends ShapeTag.Companion[OffsetDateTimeStructur
   val hints: Hints = Hints.empty
 
   // constructor using the original order from the spec
-  private def make(offsetDateTime: OffsetDateTime, offsetDateTime2: MyOffsetDateTime, offsetDateTime3: Timestamp): OffsetDateTimeStructure = OffsetDateTimeStructure(offsetDateTime, offsetDateTime2, offsetDateTime3)
+  private def make(offsetDateTime: OffsetDateTime, offsetDateTime2: MyOffsetDateTime, offsetDateTime3: OffsetDateTime): OffsetDateTimeStructure = OffsetDateTimeStructure(offsetDateTime, offsetDateTime2, offsetDateTime3)
 
   implicit val schema: Schema[OffsetDateTimeStructure] = struct[OffsetDateTimeStructure](
     offsetdatetime.required[OffsetDateTimeStructure]("offsetDateTime", _.offsetDateTime),
     MyOffsetDateTime.schema.required[OffsetDateTimeStructure]("offsetDateTime2", _.offsetDateTime2),
-    Timestamp.schema.required[OffsetDateTimeStructure]("offsetDateTime3", _.offsetDateTime3).addHints(Hints.dynamic(ShapeId("alloy", "offsetDateTimeFormat"), smithy4s.Document.obj()), Hints.dynamic(ShapeId("smithy.api", "timestampFormat"), smithy4s.Document.fromString("date-time"))),
+    offsetdatetime.required[OffsetDateTimeStructure]("offsetDateTime3", _.offsetDateTime3).addHints(Hints.dynamic(ShapeId("alloy", "offsetDateTimeFormat"), smithy4s.Document.obj()), Hints.dynamic(ShapeId("smithy.api", "timestampFormat"), smithy4s.Document.fromString("date-time"))),
   )(make).withId(id).addHints(hints)
 }
